ba9715f45d20db27a2c5c8ffbfe8a3204e61a9cf
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-03-23 14:26:39 +0100

    feat: :sparkles: accueil view

--- boiteaidee/main/views.py
+++ boiteaidee/main/views.py
@@ -7,3 +7,10 @@ def hello(request):
     return HttpResponse(f"""
         <h1>Hello Django from container!</h1>
 """)
+
+
+def accueil(request):
+
+        return HttpResponse(f"""
+        <h1>Bienvenu sur l'accueil!</h1>
+""")
